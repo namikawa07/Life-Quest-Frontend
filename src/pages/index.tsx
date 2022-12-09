9cbe2ac1e6c40e7840e9b8fbcbd872de01cfb7e1
namikawa07 <tatsuki111407@gmail.com>
2022-12-09 16:28:31 +0900

    リファクタ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,11 @@ import type { NextPage } from 'next'
 import MainLayout from '../layouts'
 import Image from 'next/image'
 import styles from './index.module.scss'
-// import Test from '../components/tsx/3DModel/Test'
-// import { YBot } from '../components/tsx/webGL/character/model/YBot'
-import ModelRender from '../components/tsx/Test'
+import ModelRender from '../components/tsx/ModelRender.js'
 
 const RouteIndex: NextPage = () => {
   return (
     <MainLayout>
-      {/*<YBot />*/}
-      {/*<Test />*/}
       <ModelRender />
       <div className={styles.background}>
         <Image src="/image.jpg" alt="logo" layout="fill" objectFit="cover" />
--- src/types.ts
+++ src/types.ts
@@ -91,10 +91,4 @@ export interface AccountDescriptionComponentType {
   description: string
 }
 
-import { AnimationNames, TextureNames } from './animationParams'
-
-export type AnimationName = typeof AnimationNames[number]
-
-export type TextureName = typeof TextureNames[number]
-
 export default TypeScript
